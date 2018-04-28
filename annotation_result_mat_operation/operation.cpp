ce9016a2241d7218f65491134fba834c7429687b
LinLiting <linliting06@live.com>
2018-04-28 15:14:59 +0800

    c

--- annotation_result_mat_operation/operation.cpp
+++ annotation_result_mat_operation/operation.cpp
@@ -1,1 +1,16 @@
 #include <matio.h>
+
+#include <string>
+
+class AnnotationOperator
+{
+public:
+	AnnotationOperator(const std::wstring &matFilePath);
+	int getNumberOfRecords();
+	int getRecord(int index, int *id, int *labeled, int *x, int *y, int *w, int *h, int *occlusion, int *out_view);
+	int updateRecord(int index, int id, int labeled, int x, int y, int w, int h, int occlusion, int out_view, const char *path);
+};
+
+AnnotationOperator::AnnotationOperator(const std::wstring& matFilePath)
+{
+}
